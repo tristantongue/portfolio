00037c290138f68d806cb37e2e47600e6a875bfb
Tristan Tongue <tristantongue@gmail.com>
2021-04-24 17:29:40 -0500

    add link to card

--- src/components/WorkPage/WorkPage.js
+++ src/components/WorkPage/WorkPage.js
@@ -37,7 +37,7 @@ function WorkPage() {
                 <WorkCard
                     title={"My Portfolio Website"}
                     caption={"A fully responsive single page React application that harnesses GSAP and React Hooks for event and scroll triggered animations. Includes custom animated SVG."}
-                    link={"VIEW REPOSITORY"}>
+                    link={<a href="https://github.com/tristantongue/portfolio/tree/master/src">VIEW REPOSITORY</a>}>
                     
                 </WorkCard>
                 <WorkCard
