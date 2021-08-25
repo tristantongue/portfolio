3063f1698b87e25c9d4764c60cb2ccce8decc110
Tristan Tongue <tristantongue@gmail.com>
2021-08-25 08:51:41 -0400

    edited landing text

--- src/components/LandingPage/IntroText/IntroText.js
+++ src/components/LandingPage/IntroText/IntroText.js
@@ -6,7 +6,7 @@ function IntroText() {
         <div className="introContainer">
         <div className="introduction">
             <h1>Tristan Tongue</h1>
-            <p>Front-end web development</p>
+            <p>Full-Stack Developer</p>
             <a href="https://www.linkedin.com/in/tristan-tongue-ba6902184/" target="_blank" rel="noreferrer" class="fa fa-linkedin"></a>
         </div>
         </div>
